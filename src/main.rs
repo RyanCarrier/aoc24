66ad81cb668ea04e9e55205358c49e037195969a
Ryan Carrier <ryan.j.carrier@gmail.com>
2024-12-06 19:09:25 +1000

    init

---
+++ src/main.rs
@@ -0,0 +1,178 @@
+use ansi_term::Style;
+use clap::Parser;
+use std::time::{Duration, Instant};
+use util::Problem;
+
+pub mod days;
+pub mod jamz;
+mod util;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value_t = 0)]
+    day: usize,
+    #[arg(short, long, default_value_t = 0)]
+    part: usize,
+    #[arg(short, long)]
+    test: bool,
+    #[arg(short, long)]
+    benchmark: bool,
+    #[arg(short, long, default_value_t = 100)]
+    iterations: usize,
+    #[arg(short, long)]
+    virgin: bool,
+}
+const YEAR: usize = 2023;
+const DAYS: [Problem; 20] = [
+    days::day1::DAY1,
+    days::day2::DAY2,
+    days::day3::DAY3,
+    days::day4::DAY4,
+    days::day5::DAY5,
+    days::day6::DAY6,
+    days::day7::DAY7,
+    days::day8::DAY8,
+    days::day9::DAY9,
+    days::day10::DAY10,
+    days::day11::DAY11,
+    days::day12::DAY12,
+    days::day13::DAY13,
+    days::day14::DAY14,
+    days::day15::DAY15,
+    days::day16::DAY16,
+    days::day17::DAY17,
+    days::day18::DAY18,
+    days::day19::DAY19,
+    days::day20::DAY20,
+];
+const JAMZ: [Problem; 1] = [jamz::day1::DAY1];
+
+fn main() {
+    let args = Args::parse();
+    if args.benchmark {
+        benchmark(args);
+        return;
+    }
+    let problems = if args.virgin {
+        JAMZ.to_vec()
+    } else {
+        DAYS.to_vec()
+    };
+    if args.day == 0 {
+        //just assume
+        for day in 1..=problems.len() {
+            run_specific(&problems[day - 1], &args);
+        }
+        return;
+    }
+    run_specific(&problems[args.day - 1], &args);
+}
+
+fn run_specific(problem: &Problem, args: &Args) {
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .paint("=== Day ".to_owned() + &problem.day.to_string() + " ==="),
+    );
+    let input = if args.test {
+        (problem
+            .test_data
+            .expect("Asked for test data, but there was none set"))()
+        .split('\n')
+        .map(|x| x.to_owned())
+        .collect()
+    } else {
+        util::get_input_data(YEAR, problem.day)
+    };
+    let start = Instant::now();
+    if args.part == 0 || args.part == 1 {
+        print_result(problem, args.virgin, 1, args.test, (problem.part1)(&input));
+    }
+    let part1_duration = start.elapsed();
+    if args.part == 0 || args.part == 2 {
+        print_result(problem, args.virgin, 2, args.test, (problem.part2)(&input));
+    }
+    let total_duration = start.elapsed();
+    println!(
+        "Completed in {}\t(p1:{}, p2:{})",
+        util::format_duration(total_duration),
+        util::format_duration(part1_duration),
+        util::format_duration(total_duration - part1_duration)
+    );
+}
+fn print_result(problem: &Problem, virgin: bool, part: usize, test: bool, result: String) {
+    println!(
+        "day{}part{}{}{}:\t{}",
+        problem.day,
+        part,
+        if virgin { "-JAMZ" } else { "" },
+        if test { "-TEST" } else { "" },
+        result
+    );
+}
+
+fn benchmark(args: Args) {
+    let runs: usize = args.iterations;
+    let problems = if args.virgin {
+        println!("Running virgin mode activated");
+        JAMZ.to_vec()
+    } else {
+        println!("Running default high speed computations");
+        DAYS.to_vec()
+    };
+    let range = if args.day == 0 {
+        0..problems.len()
+    } else {
+        (args.day - 1)..(args.day)
+    };
+    let max = if args.day == 0 {
+        problems.len()
+    } else {
+        args.day
+    };
+    let mut data = vec![vec![String::new()]; max];
+    print!("Getting input data... ");
+    for day in range.clone() {
+        print!("{}... ", day + 1);
+        data[day] = util::get_input_data(YEAR, day + 1);
+    }
+    println!("");
+    println!(
+        "{}, {} iterations",
+        Style::new().bold().paint("Day durations"),
+        runs
+    );
+    println!("Day\t\tPart1\tPart2\tTotal");
+    let mut part1_durations = vec![Duration::default(); max];
+    let mut part2_durations = vec![Duration::default(); max];
+    let mut total_duration = Duration::default();
+    for day in range.clone() {
+        let start = Instant::now();
+        for _ in 0..runs {
+            (problems[day].part1)(&data[day]);
+        }
+        part1_durations[day] = start.elapsed().div_f64(runs as f64);
+        let start = Instant::now();
+        for _ in runs..(runs * 2) {
+            (problems[day].part2)(&data[day]);
+        }
+        part2_durations[day] = start.elapsed().div_f64(runs as f64);
+        println!(
+            "Day {}:\t\t{}\t{}\t{}",
+            day + 1,
+            util::format_duration(part1_durations[day]),
+            util::format_duration(part2_durations[day]),
+            Style::new().bold().paint(util::format_duration(
+                part1_durations[day] + part2_durations[day]
+            ))
+        );
+        total_duration += part1_durations[day] + part2_durations[day];
+    }
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .paint(format!("Total:\t{}", util::format_duration(total_duration)))
+    );
+}
---
+++ src/util.rs
@@ -0,0 +1,171 @@
+use std::{collections::BinaryHeap, fs, path::Path, sync::Arc, time::Duration};
+
+use reqwest::{cookie::Jar, Url};
+#[derive(Clone, Copy)]
+pub struct Problem {
+    pub day: usize,
+    pub part1: fn(input: &Vec<String>) -> String,
+    pub part2: fn(input: &Vec<String>) -> String,
+    pub test_data: Option<fn() -> &'static str>,
+}
+#[allow(dead_code)]
+pub static TRANSFORMS: [[isize; 2]; 8] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
+
+pub fn format_duration(d: Duration) -> String {
+    if d.as_millis() > 1000 {
+        return format!("{:.1}s", d.as_millis() as f64 / 1000 as f64);
+    }
+    if d.as_micros() > 1000 {
+        return format!("{:.1}ms", d.as_micros() as f64 / 1000 as f64);
+    }
+    if d.as_nanos() > 1000 {
+        return format!("{:.1}μs", d.as_nanos() as f64 / 1000 as f64);
+    }
+    format!("{}ns", d.as_nanos())
+}
+
+pub fn get_input_data(year: usize, day: usize) -> Vec<String> {
+    // get input data from aoc using cookie
+    if !Path::new("./cookie.txt").exists() {
+        panic!("cookie in a file called cookie.txt plzx");
+    }
+    let session_id = fs::read_to_string("./cookie.txt").unwrap();
+    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day)
+        .parse::<Url>()
+        .unwrap();
+    let cookie = format!("session={}", session_id);
+    let jar = Jar::default();
+    jar.add_cookie_str(&cookie, &url);
+    let client = reqwest::blocking::ClientBuilder::new()
+        .cookie_provider(Arc::new(jar))
+        .build()
+        .unwrap();
+
+    let resp = client
+        .get(url)
+        // .header("cookie", format!("session={}", cookie))
+        .send()
+        .unwrap();
+    if !resp.status().is_success() {
+        panic!(
+            "Failed to get input data, {:?}\n{:?}",
+            resp.status(),
+            resp.text().unwrap()
+        );
+    }
+    let body = resp.text().unwrap();
+    body.split('\n').map(|x| x.to_owned()).collect()
+}
+
+#[allow(dead_code)]
+pub struct Graph {
+    pub verticies: usize,
+    pub arcs: Vec<Vec<VectorArc>>,
+}
+pub struct VectorArc {
+    pub from: usize,
+    pub to: usize,
+    pub weight: usize,
+}
+#[allow(dead_code)]
+impl VectorArc {
+    pub fn new(from: usize, to: usize, weight: usize) -> Self {
+        Self { from, to, weight }
+    }
+}
+#[derive(Copy, Clone, Eq, Debug)]
+#[allow(dead_code)]
+pub struct Vertex {
+    pub index: usize,
+    pub dist: usize,
+}
+impl PartialEq for Vertex {
+    fn eq(&self, other: &Self) -> bool {
+        self.dist == other.dist
+    }
+}
+impl Ord for Vertex {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.dist.cmp(&other.dist).reverse()
+        // other.dist.cmp(&self.dist)
+    }
+}
+
+impl PartialOrd for Vertex {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+#[allow(dead_code)]
+impl Graph {
+    pub fn dijkstra(&self, from: usize, to: usize) -> usize {
+        let mut dist = vec![usize::MAX; self.verticies];
+        let mut pq: BinaryHeap<Vertex> = BinaryHeap::new();
+        dist[from] = 0;
+        pq.push(Vertex {
+            index: from,
+            dist: 0,
+        });
+        while let Some(next) = pq.pop() {
+            println!("{:?}", next);
+            if next.index == to {
+                return next.dist;
+            }
+            for arc in self.arcs[next.index].iter() {
+                let new_dist = next.dist + arc.weight;
+                println!(
+                    "{}=={}->{} newdist {} dist[] {}",
+                    next.index, arc.from, arc.to, new_dist, dist[arc.to]
+                );
+                if new_dist < dist[arc.to] {
+                    dist[arc.to] = new_dist;
+                    pq.push(Vertex {
+                        index: arc.to,
+                        dist: new_dist,
+                    });
+                }
+            }
+            println!("{:?}", pq);
+        }
+        0
+    }
+}
+//test for dijkstra
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dijkstra() {
+        let graph = Graph {
+            verticies: 6,
+            arcs: vec![
+                vec![VectorArc::new(0, 1, 1), VectorArc::new(0, 2, 12)],
+                vec![
+                    VectorArc::new(1, 2, 9),
+                    VectorArc::new(1, 3, 3),
+                    VectorArc::new(1, 2, 1),
+                ],
+                vec![VectorArc::new(2, 4, 5)],
+                vec![
+                    VectorArc::new(3, 2, 4),
+                    VectorArc::new(3, 4, 13),
+                    VectorArc::new(3, 5, 15),
+                ],
+                vec![VectorArc::new(4, 5, 4)],
+                vec![],
+            ],
+        };
+        assert_eq!(graph.dijkstra(0, 5), 11);
+        assert_eq!(graph.dijkstra(0, 2), 2);
+    }
+}
